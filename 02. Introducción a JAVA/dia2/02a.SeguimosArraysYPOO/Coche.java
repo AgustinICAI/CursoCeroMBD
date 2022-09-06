e009e69c2e107da9d28c30eea8348c385be063e4
AgustinICAI <a.g.lamela@gmail.com>
2022-09-06 11:40:27 +0200

    metiendo ejemplos con atributos de clase y métodos de clase

---
+++ 02. Introducción a JAVA/dia2/02a.SeguimosArraysYPOO/Coche.java
@@ -0,0 +1,44 @@
+public class Coche {
+    String marca;
+    String modelo;
+    int potencia;
+
+    public Coche(String marca, String modelo, int potencia) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.potencia = potencia;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", potencia=" + potencia +
+                '}';
+    }
+}
